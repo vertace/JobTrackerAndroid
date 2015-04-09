00681ed0864df87e1173194b07991eec777bceb7
vertace <vasanth@vertace.com>
2015-04-09 19:47:43 +0530

    Vasanth Checkin

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -103,12 +103,12 @@ public final class R {
 		public static final int common_signin_btn_text_normal_light = 0x7f020053;
 		public static final int common_signin_btn_text_pressed_dark = 0x7f020054;
 		public static final int common_signin_btn_text_pressed_light = 0x7f020055;
-		public static final int ic_plusone_medium_off_client = 0x7f020060;
-		public static final int ic_plusone_small_off_client = 0x7f020061;
-		public static final int ic_plusone_standard_off_client = 0x7f020062;
-		public static final int ic_plusone_tall_off_client = 0x7f020063;
-		public static final int powered_by_google_dark = 0x7f020072;
-		public static final int powered_by_google_light = 0x7f020073;
+		public static final int ic_plusone_medium_off_client = 0x7f020062;
+		public static final int ic_plusone_small_off_client = 0x7f020063;
+		public static final int ic_plusone_standard_off_client = 0x7f020064;
+		public static final int ic_plusone_tall_off_client = 0x7f020065;
+		public static final int powered_by_google_dark = 0x7f020076;
+		public static final int powered_by_google_light = 0x7f020077;
 	}
 	public static final class id {
 		public static final int adjust_height = 0x7f0b0017;
@@ -178,9 +178,9 @@ public final class R {
 		public static final int create_calendar_message = 0x7f0d003a;
 		public static final int create_calendar_title = 0x7f0d003b;
 		public static final int decline = 0x7f0d003c;
-		public static final int store_picture_message = 0x7f0d005b;
-		public static final int store_picture_title = 0x7f0d005c;
-		public static final int wallet_buy_button_place_holder = 0x7f0d0064;
+		public static final int store_picture_message = 0x7f0d005e;
+		public static final int store_picture_title = 0x7f0d005f;
+		public static final int wallet_buy_button_place_holder = 0x7f0d0066;
 	}
 	public static final class style {
 		public static final int Theme_IAPTheme = 0x7f0e00b4;
