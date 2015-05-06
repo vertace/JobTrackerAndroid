cb5405703666db28cbdca6742c30c1ad21a7dce7
Poovalagan <poovalagan@vertace.com>
2015-05-06 20:32:43 +0530

    Upload

---
+++ app/src/main/java/com/tt/data/RfeViewModel.java
@@ -0,0 +1,15 @@
+package com.tt.data;
+
+/**
+ * Created by BS-308 on 5/6/2015.
+ */
+public class RfeViewModel {
+
+    public int ID;
+    public String FullName;
+    public String Address;
+    public int EmployeeID;
+    public String Description;
+    public String Code;
+    public String RFENumber;
+}
