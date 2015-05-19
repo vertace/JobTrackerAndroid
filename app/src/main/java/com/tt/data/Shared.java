140ed02202a7eb431a9661d7aa4d2bed17be09c6
Poovalagan <poovalagan@vertace.com>
2015-05-19 18:06:37 +0530

    Upload

--- app/src/main/java/com/tt/data/Shared.java
+++ app/src/main/java/com/tt/data/Shared.java
@@ -1,6 +1,7 @@
 package com.tt.data;
 
 import android.graphics.Bitmap;
+import android.net.Uri;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +11,8 @@ public class Shared {
 
     //public static String WebAPI = "http://sunsigns.azurewebsites.net/";
     //public static String WebAPI = "http://emtracker.specialeffect.co.in/";
-    //public static String WebAPI = "http://192.168.0.104:1989/";
-    public static String WebAPI = "http://jobtrack.azurewebsites.net/";
+     //public static String WebAPI = "http://192.168.0.105:1989/";
+  public static String WebAPI = "http://jobtrack.azurewebsites.net/";
     public static String LoginAPI = WebAPI + "/Employee/Login";
     public static String TaskListAPI = WebAPI + "/Task/GetTasksForEmployeeNew";
     public static String TaskDetailAPI = WebAPI + "/Task/GetTaskDetail";
@@ -23,13 +24,17 @@ public class Shared {
 
     public static String EmployeeListAPI = WebAPI + "Employee/AllEmployees";
     public static String UploadShopPhotoAPI = WebAPI + "Task/UploadShopPhotoAzure";
-public static String RfeListAPI=WebAPI+"Task/GetRfeList";
+
 
     public static String SaveRegistrationID=WebAPI+"Task/SaveRegistrationID";
+
+    public static String RfeListAPI=WebAPI+"Task/GetRfeList";
+    public static String RfeTaskListAPI = WebAPI + "/Task/GetRfeTaskList";
 
     public static EmployeeViewModel LoggedInUser;
     public static List<TaskViewModel> TaskList;
     public static List<RfeViewModel> RfeList;
+    public static  RfeViewModel Selected_Rfe;
 
     public static List<TaskLineItemViewModel> TaskDetail;
     public static MeasurementPhoto MeasurementUploadPhoto;
@@ -70,6 +75,8 @@ public static String RfeListAPI=WebAPI+"Task/GetRfeList";
     public static String UploadResponse;
     public static String ShopUploadResponse;
     public static ArrayList<MapShopSortViewModel> MapSortByShop;
+
+    public static Uri FileUri;
 
     public static String GCM_SENDER_ID = "85852809473";
     public static String hideMenu;
